d502289027f6a29a85ea09e05512292cd9ec4722
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-08-04 01:53:59 +0900

    멘토 멘티 리스트 객체 생성

---
+++ src/components/UserType/userTypeList.ts
@@ -0,0 +1,22 @@
+// Assets
+import mento from "../../assets/mento.svg";
+import mentee from "../../assets/mentee.svg";
+
+const userTypeSelect = [
+  {
+    id: 1,
+    iconSrc: mento,
+    name: "멘토 회원",
+    mainDesc: "피드백 해주고 내실력도 up!",
+    subDesc: "꿈나무들을 위해 이력서 피드백 해주고 싶어요",
+  },
+  {
+    id: 2,
+    iconSrc: mentee,
+    name: "멘티 회원",
+    mainDesc: "이력서 작성하고",
+    subDesc: "현직자의 피드백으로 쑥쑥 성장하고 싶어요",
+  },
+];
+
+export default userTypeSelect;

5a66035155fab65c8d49350c92f9ac0ee33a3d40
gunyongbok <qhrrjsdyd123@gmail.com>
2023-08-17 23:21:26 +0900

    test

--- src/utility/ValidatePw.ts
+++
@@ -1,6 +0,0 @@
-const validatePassword = (pw: string) => {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    return passwordRegex.test(pw);
-};
-
-export default validatePassword;
--- src/utility/ValidateRePw.ts
+++
@@ -1,5 +0,0 @@
-const validateRePassword = (pw: string, repw: string): boolean => {
-  return pw === repw;
-};
-
-export default validateRePassword;

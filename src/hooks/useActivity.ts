55cfc37471712cf6201f8f0c57cd5eb2d40bae0e
[LK1] <[ssomcandy777@gmail.com]>
2023-09-12 17:36:31 +0900

    Feat: Activity boolean 검증 커스텀 훅 추가

---
+++ src/hooks/useActivity.ts
@@ -0,0 +1,12 @@
+// React
+import { useState } from "react";
+
+const useAcitivity = () => {
+  const [activityBtn, setAcitivityBtn] = useState(false);
+
+  const updateStatusBtn = (status: boolean) => setAcitivityBtn(status);
+
+  return [activityBtn, updateStatusBtn];
+};
+
+export default useAcitivity;

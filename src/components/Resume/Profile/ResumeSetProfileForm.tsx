642f700534c27f503bb7a846eb1f2d969eda4364
[LK1] <[ssomcandy777@gmail.com]>
2023-09-18 13:55:29 +0900

    Fix: 키워드 오류 해결

--- src/components/Resume/Profile/ResumeSetProfileForm.tsx
+++ src/components/Resume/Profile/ResumeSetProfileForm.tsx
@@ -223,13 +223,22 @@ const ResumeSetProfileForm = ({ updateStatusBtn }: Props) => {
     e: React.KeyboardEvent<HTMLInputElement>
   ) => {
     if (e.key === "Enter") {
-      if (currentKeyword.length >= 6)
+      if (currentKeyword.trim().length < 2) {
+        setCurrentKeyword("");
+        return alert("키워드는 2글자 이상 작성 부탁드립니다.");
+      }
+      if (currentKeyword.length >= 6) {
+        setCurrentKeyword("");
         return alert("6글자 이내로 작성 부탁드립니다.");
-      if (myKeywords.length >= 5)
+      }
+      if (myKeywords.length >= 5) {
+        setCurrentKeyword("");
         return alert("키워드는 5개까지만 추가가능 합니다.");
+      }
       setMyKeywords([...myKeywords, currentKeyword]);
       setCurrentKeyword("");
     }
+    return;
   };
 
   // 키워드 삭제 함수

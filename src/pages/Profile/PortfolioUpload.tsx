a50447b3ea2f0c65319fe539fe4e0c7ddb2ad70a
sstipdev <whitecandy777@naver.com>
2023-09-21 17:19:45 +0900

    업로드 페이지 코드 리팩토링

--- src/pages/Profile/PortfolioUpload.tsx
+++ src/pages/Profile/PortfolioUpload.tsx
@@ -1,6 +1,3 @@
-// React
-import { useEffect } from "react";
-
 // Custom Hook
 import useAcitivity from "../../hooks/useActivity";
 
@@ -9,14 +6,14 @@ import styled from "styled-components";
 
 // Components
 import ResumStatusBar from "../../components/Resume/ResumStatusBar";
-import UploadPortfolioForm from "../../components/Resume/Profile/UploadPortfolioForm";
+import UploadPortfolioForm from "../../components/Resume/Portfolio/UploadPortfolioForm";
 
 const PortfolioUploadMain = styled.div`
   width: 100%;
 `;
 
 const PortfolioUpload = () => {
-  const [activityBtn, updateStatusBtn] = useAcitivity();
+  const [activityBtn] = useAcitivity();
 
   return (
     <PortfolioUploadMain>

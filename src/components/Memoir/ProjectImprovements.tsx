64b2f4f5737ea0f276ac8b957fb3113f29999811
sstipdev <whitecandy777@naver.com>
2023-09-22 17:18:47 +0900

    Feat: 임시 회고록 2 페이지 기본셋팅

---
+++ src/components/Memoir/ProjectImprovements.tsx
@@ -0,0 +1,8 @@
+// React
+import { useState } from "react";
+
+const ProjectImprovements = () => {
+  return <div></div>;
+};
+
+export default ProjectImprovements;
---
+++ src/pages/Profile/Improvement.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+// Custom Hooks
+import useAcitivity from "../../hooks/useActivity";
+
+// Components
+import ResumStatusBar from "../../components/Resume/ResumStatusBar";
+import ProejctOverview from "../../components/Memoir/ProjectOverview";
+
+const Improvements = () => {
+  const [activityBtn, updateStatusBtn] = useAcitivity();
+
+  useEffect(() => {
+    console.log(activityBtn);
+  }, [activityBtn]);
+  return <div></div>;
+};
+
+export default Improvements;

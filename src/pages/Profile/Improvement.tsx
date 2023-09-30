c292bf8316ddf51a3fae1a55ec5aef8caec5426e
lk1 <whitecandy777@naver.com>
2023-09-30 11:37:14 +0900

    회고록 2스텝 페이지 헤더 적용

--- src/pages/Profile/Improvement.tsx
+++ src/pages/Profile/Improvement.tsx
@@ -4,16 +4,21 @@ import { useEffect } from "react";
 import useAcitivity from "../../hooks/useActivity";
 
 // Components
-// import ResumStatusBar from "../../components/Resume/ResumStatusBar";
+import ResumStatusBar from "../../components/Resume/ResumStatusBar";
 // import ProejctOverview from "../../components/Memoir/ProjectOverview";
 
 const Improvements = () => {
   const [activityBtn] = useAcitivity();
-
-  useEffect(() => {
-    console.log(activityBtn);
-  }, [activityBtn]);
-  return <div></div>;
+  return (
+    <div>
+      <ResumStatusBar
+        background="2"
+        MobileLineWidth="100%"
+        profileTitle="포트폴리오 업로드"
+        activityBtn={activityBtn}
+      />
+    </div>
+  );
 };
 
 export default Improvements;

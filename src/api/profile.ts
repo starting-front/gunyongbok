5c793a69b1a85d1aa237f8b5cf7958fdd7e11316
lk1 <whitecandy777@naver.com>
2023-10-09 19:13:31 +0900

    프로필 설정 api 테스트 기능 추가

---
+++ src/api/profile.ts
@@ -0,0 +1,23 @@
+// Promise
+import axios from "axios";
+
+export interface Profile {
+  name: string;
+  email: string;
+  tel: string;
+  field: string;
+  job: string;
+  introduce: string;
+  keywords: string[];
+}
+
+export const editProfileInfo = (args: Profile) => {
+  const RESUME_ID = "test";
+  if (args) {
+    const res = axios.post(
+      `${import.meta.env.VITE_REACT_API_BASE_URL}/api/resumes${RESUME_ID}`
+    );
+    return res;
+  }
+  return null;
+};
